88f4275bb5f48cad8ba43b9d3ab303263126871a
Yukio Yamamoto <yukio@planeta.sci.isas.jaxa.jp>
2024-10-04 21:10:50 +0900

    Removed white spaces

--- time_exceptions.py
+++ time_exceptions.py
@@ -12,6 +12,7 @@ class OctetSizeException(Exception):
     def __init__(self, message):
         self.message = message
 
+
 class ReservedForFutureUse(Exception):
     def __init__(self, message):
         self.message = message
